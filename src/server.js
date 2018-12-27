550ad54c85fe1e47b5619676dcc565ea10cb9373
Aaron Adams <aaron.adams@intelie.com.br>
2018-12-26 21:58:59 -0600

    init-3

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const createConnection = require("./connection");
 const { seedRigs, seedFleets, backfillFleetsWithRigs } = require("./seeder/seeder");
 
 const startServer = async () => {
-  const PORT = 3000;
+  const PORT = process.env.PORT || 3000;
   const app = express();
 
   app.use(cors());
